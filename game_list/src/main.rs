9d0e6896e77e7fd7564b3540547547e8c8ec22d7
nuiben <ben.garrett.porter@gmail.com>
2023-04-29 19:47:23 -0400

    Rust API 04-29-23

---
+++ game_list/src/main.rs
@@ -0,0 +1,21 @@
+
+use mongodb::{Client, options::ClientOptions};
+
+// Parse a connection string into an options struct.
+let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+
+// Manually set an option.
+client_options.app_name = Some("My App".to_string());
+
+// Get a handle to the deployment.
+let client = Client::with_options(client_options)?;
+
+// List the names of the databases in that deployment.
+for db_name in client.list_database_names(None, None).await? {
+    println!("{}", db_name);
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
